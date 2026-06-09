9398c5bb665d7c39dc776cd3334d9a88519db0ce
mask2942002 <muslihkentiba15@gmail.com>
2026-06-09 05:13:29 +0000

    my java code

---
+++ tutorial_2/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        student s1 = new student("Mosleh", 21, 3.8);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
